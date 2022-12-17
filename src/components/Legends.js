8bf4c67e8da351a83b2f8b3fb734fa351f269058
YifeiJing <jingyifei1998@qq.com>
2022-12-17 17:23:35 +0900

    Add photo for 2th competition

--- src/components/Legends.js
+++ src/components/Legends.js
@@ -3,6 +3,9 @@ import p1 from './img/cup1_1.jpeg'
 import p2 from './img/cup1_2.jpeg'
 import p3 from './img/cup1_3.jpeg'
 import p4 from './img/cup1_4.JPG'
+import cup2_1 from './img/cup2_1.jpeg'
+import cup2_2 from './img/cup2_2.jpeg'
+import cup2_3 from './img/cup2_3.jpeg'
 
 const Legends = (props) => {
 	const handleReturn = () => {
@@ -13,6 +16,25 @@ const Legends = (props) => {
 		<div className='w3-padding-16'>
 			<button className="w3-button w3-teal w3-left" onClick={handleReturn}>Return</button>
 		</div>
+		<div className="w3-padding-64">
+			<h2>第二届可乐杯优胜者</h2>
+			<ul className="w3-ul">
+				<li>王浩哲</li>
+				<li>刘博文</li>
+				<li>tibbit</li>
+				<li>阿牧</li>
+			</ul>
+		</div>
+		<div className="w3-row">
+			<img src={cup2_1} style={{width:"100%"}}></img>
+		</div>
+		<div className="w3-row">
+			<img src={cup2_2} style={{width:"100%"}}></img>
+		</div>
+		<div className="w3-row">
+			<img src={cup2_3} style={{width:"100%"}}></img>
+		</div>
+
 		<div className="w3-padding-64">
 			<h2>第一届可乐杯优胜者</h2>
 			<ul className="w3-ul">
